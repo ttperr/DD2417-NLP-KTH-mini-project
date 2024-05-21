92c8150cda073e76d1a4fa25d190e0a41fc8b951
Romain Darous <romain.darousfr@gmail.com>
2024-05-21 20:12:07 +0200

    Building an appropriate dataset

--- char_dataset.py
+++ char_dataset.py
@@ -11,32 +11,49 @@ class CharDataset(Dataset) :
     char_to_id = {}
     id_to_char = {}
     PADDING_SYMBOL = '<PAD>'
-    char_to_id[PADDING_SYMBOL] = 0
+    BOQ = '<BOQ>'
 
-    def __init__(self, file_path, n) :
+    char_to_id[PADDING_SYMBOL] = 0
+    char_to_id[BOQ] = 1
+    id_to_char[0] = PADDING_SYMBOL
+    id_to_char[1] = BOQ
+
+    def __init__(self, dataset, max_len) :
         self.datapoints = []
         self.labels = []
-        chars = []
-        try :
-            # First read the dataset to find all the unique characters
-            with open(file_path,'r',encoding='utf-8') as f :
-                contents = f.read()
-            for char in contents:
-                if char not in self.char_to_id:
-                    self.char_to_id[char] = len(self.id_to_char)
-                    self.id_to_char[len(self.id_to_char)] = char
-                chars.append( self.char_to_id[char] )
-            # Then go through all the chars and chunk them up into datapoints
-            k = 0
-            while k < len(chars)-n:
-                for i in range(1, n+1):
-                    self.datapoints.append([c for c in chars[k:i+k]+[0]*(n-i)])
-                    self.labels.append(chars[i+k])
-                k += n
-        except FileNotFoundError:
-            print(f"File not found: {file_path}")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+
+        for seq in dataset:
+            words = seq.split(" ")
+
+            lbl_words = " ".join(words[-2:])
+            lbl_chars = [c for c in lbl_words] + [self.BOQ]
+
+            feat_words = " ".join(words[:-2])
+            feat_chars = [c for c in feat_words]
+
+            # Updating vocabulary
+            lbl_ids = []
+            feat_ids = []
+
+            for c in lbl_chars:
+                if c not in self.char_to_id.keys():
+                    self.char_to_id[c] = len(self.id_to_char)
+                    self.id_to_char[len(self.id_to_char)] = c
+                lbl_ids.append(self.char_to_id[c])
+
+            for c in feat_chars:
+                if c not in self.char_to_id.keys():
+                    self.char_to_id[c] = len(self.id_to_char)
+                    self.id_to_char[len(self.id_to_char)] = c
+                feat_ids.append(self.char_to_id[c])
+                        
+            # Building features and labels
+            for i in range(len(lbl_ids)) :
+                self.datapoints.append(feat_ids[-max_len + i:] + lbl_ids[:i])
+                self.labels.append(lbl_ids[i])
+        
+        print("finished")
+        print(self.labels)
 
     def __len__(self) :
         return len(self.datapoints)
